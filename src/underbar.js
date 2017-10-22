cef8192a1d6fac86882ea7d9634e347856d07881
James Byrd <jd3yrd@gmail.com>
2017-10-22 03:37:42 -0400

    Complete map pluck & 50% uniq

--- src/underbar.js
+++ src/underbar.js
@@ -103,6 +103,17 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+
+    var result = [];
+
+    _.each(array, function(item, index){
+      if(!result.includes(item)){
+        result.push(item);
+      }
+    });
+
+    return result;
+
   };
 
 
@@ -111,6 +122,15 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+
+    var result = [];
+
+    _.each(collection, function(item){
+      result.push(iterator(item));
+    });
+
+    return result;
+
   };
 
   /*
@@ -152,6 +172,8 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+
+
   };
 
   // Determine if the array or object contains a given value (using `===`).
