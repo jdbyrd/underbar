742c359ad5c02a70235df6fbc178fc0f3268b544
James Byrd <jd3yrd@gmail.com>
2017-10-24 07:07:36 -0400

    Complete Contains Every Some

--- src/underbar.js
+++ src/underbar.js
@@ -213,13 +213,35 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    return _.reduce(collection, function(passed, item){
+      if(passed === false){
+        return false;
+      }
+      if(typeof iterator === 'function'){
+        if(iterator(item)){
+          return true;
+        }else{
+          return false;
+        }
+      }else{
+        return item;
+      }
+
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
-  };
+    return !_.every(collection, function(item){
+      if(typeof iterator === 'function'){
+        return !iterator(item);
+      }else{
+        return !item;
+      }
+    })
+    };
 
 
   /**
